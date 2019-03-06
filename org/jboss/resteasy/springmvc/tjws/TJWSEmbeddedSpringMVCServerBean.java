7c087a06bdeaf9a1bef0f91467b54edc605cba6b
Weinan Li <l.weinan@gmail.com>
2019-03-06 22:19:22 +0800

    save progress

--- org/jboss/resteasy/springmvc/tjws/TJWSEmbeddedSpringMVCServerBean.java
+++ org/jboss/resteasy/springmvc/tjws/TJWSEmbeddedSpringMVCServerBean.java
@@ -5,86 +5,71 @@ import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.InitializingBean;
 
 /**
-* @author <a href="mailto:sduskis@gmail.com">Solomn Duskis</a>
-* @version $Revision: 1 $
-* 
-*/
+ * @author <a href="mailto:sduskis@gmail.com">Solomn Duskis</a>
+ * @version $Revision: 1 $
+ */
 
 public class TJWSEmbeddedSpringMVCServerBean implements InitializingBean,
-      DisposableBean, FactoryBean<TJWSEmbeddedSpringMVCServer>
-{
+      DisposableBean, FactoryBean<TJWSEmbeddedSpringMVCServer> {
    private String applicationContextConfiguration;
    private int port;
-   private String context = ""; 
+   private String context = "";
 
    private TJWSEmbeddedSpringMVCServer server;
 
-   public String getApplicationContextConfiguration()
-   {
+   public String getApplicationContextConfiguration() {
       return applicationContextConfiguration;
    }
 
    public void setApplicationContextConfiguration(
-         String applicationContextConfiguration)
-   {
+         String applicationContextConfiguration) {
       this.applicationContextConfiguration = applicationContextConfiguration;
    }
 
-   public int getPort()
-   {
+   public int getPort() {
       return port;
    }
 
-   public void setPort(int port)
-   {
+   public void setPort(int port) {
       this.port = port;
    }
 
-   public TJWSEmbeddedSpringMVCServer getServer()
-   {
+   public TJWSEmbeddedSpringMVCServer getServer() {
       return server;
    }
 
-   public void setServer(TJWSEmbeddedSpringMVCServer server)
-   {
+   public void setServer(TJWSEmbeddedSpringMVCServer server) {
       this.server = server;
    }
 
-   public String getContext()
-   {
+   public String getContext() {
       return context;
    }
 
-   public void setContext(String context)
-   {
+   public void setContext(String context) {
       this.context = context;
    }
 
-   public void afterPropertiesSet() throws Exception
-   {
+   public void afterPropertiesSet() throws Exception {
       server = new TJWSEmbeddedSpringMVCServer(
             this.applicationContextConfiguration, this.port, context);
       server.start();
    }
 
-   public void destroy() throws Exception
-   {
+   public void destroy() throws Exception {
       server.stop();
       server = null;
    }
 
-   public TJWSEmbeddedSpringMVCServer getObject() throws Exception
-   {
+   public TJWSEmbeddedSpringMVCServer getObject() throws Exception {
       return server;
    }
 
-   public Class<TJWSEmbeddedSpringMVCServer> getObjectType()
-   {
+   public Class<TJWSEmbeddedSpringMVCServer> getObjectType() {
       return TJWSEmbeddedSpringMVCServer.class;
    }
 
-   public boolean isSingleton()
-   {
+   public boolean isSingleton() {
       return true;
    }
 
